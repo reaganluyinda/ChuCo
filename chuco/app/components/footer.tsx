44bb7b768d3add24a07649ab1cd8c43bb4cf7f72
reagan luyinda <luyindareagan@gmail.com>
2026-07-26 19:13:20 +0300

    Feat; create Footer component

---
+++ chuco/app/components/footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="pt-8 pb-4 bg-[#212529] text-white text-center">
+      <div className="flex flex-col md:flex-row justify-center items-center space-y-1 md:space-y-0 md:space-x-2">
+        <p>&copy; {new Date().getFullYear()} ChurchConnect.</p>
+        <p>All rights reserved.</p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
